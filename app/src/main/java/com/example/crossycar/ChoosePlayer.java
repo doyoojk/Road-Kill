17c260ef0ec70477091a8f68c0e52e3753cd9f84
Jamie Kim <77351173+doyoojk@users.noreply.github.com>
2023-02-12 15:34:43 -0500

    added button to temp game screen from player configuration

--- app/src/main/java/com/example/crossycar/ChoosePlayer.java
+++ app/src/main/java/com/example/crossycar/ChoosePlayer.java
@@ -2,13 +2,30 @@ package com.example.crossycar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChoosePlayer extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooseplayer);
+
+        button = findViewById(R.id.choose_player_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGameScreen();
+            }
+        });
+    }
+
+    public void openGameScreen() {
+        Intent GameScreen = new Intent(this, GameScreen.class);
+        startActivity(GameScreen);
     }
 }
