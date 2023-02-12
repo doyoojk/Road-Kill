485d48296596311be541659535a051a82d64dbd6
Jamie Kim <77351173+doyoojk@users.noreply.github.com>
2023-02-12 14:42:26 -0500

    implemented game main screen

---
+++ app/src/main/java/com/example/crossycar/ChoosePlayer.java
@@ -0,0 +1,14 @@
+package com.example.crossycar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChoosePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chooseplayer);
+    }
+}
--- app/src/main/java/com/example/crossycar/MainActivity.java
+++ app/src/main/java/com/example/crossycar/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.crossycar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.game_start_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openChoosePlayer();
+            }
+        });
+    }
+
+    public void openChoosePlayer() {
+        Intent ChoosePlayer = new Intent(this, ChoosePlayer.class);
+        startActivity(ChoosePlayer);
     }
 }
