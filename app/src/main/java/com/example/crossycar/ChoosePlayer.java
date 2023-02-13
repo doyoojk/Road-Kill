4d2e446d3d48e247d589199d0ff188572b870d3c
Jamie Kim <77351173+doyoojk@users.noreply.github.com>
2023-02-13 00:30:43 -0500

    Finished implementation for player configuration and game screen.

--- app/src/main/java/com/example/crossycar/ChoosePlayer.java
+++ app/src/main/java/com/example/crossycar/ChoosePlayer.java
@@ -3,29 +3,83 @@ package com.example.crossycar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+//import android.graphics.RadialGradient;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class ChoosePlayer extends AppCompatActivity {
-    private Button button;
+//    private Button button;
+    private RadioGroup carSelectionGroup;
+    private RadioGroup difficultySelectionGroup;
+    private EditText playerNameInput;
+    private Button saveButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chooseplayer);
 
-        button = findViewById(R.id.choose_player_button);
-        button.setOnClickListener(new View.OnClickListener() {
+//        button = findViewById(R.id.choose_player_button);
+        carSelectionGroup = findViewById(R.id.car_selection_group);
+        difficultySelectionGroup = findViewById(R.id.difficulty_selection_group);
+        playerNameInput = findViewById(R.id.player_name_input);
+        saveButton = findViewById(R.id.save_button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openGameScreen();
+//            }
+//        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openGameScreen();
+                // Get the data from the input fields
+                int selectedCarId = carSelectionGroup.getCheckedRadioButtonId();
+                int selectedDifficultyId = difficultySelectionGroup.getCheckedRadioButtonId();
+                String playerName = playerNameInput.getText().toString().trim();
+
+                // Validate the input
+                if (playerName.isEmpty()) {
+                    playerNameInput.setError("Name cannot be empty");
+                    return;
+                }
+
+                // Determine the selected car
+                String carSelection = "Car 1";
+                if (selectedCarId == R.id.car_2) {
+                    carSelection = "Car 2";
+                } else if (selectedCarId == R.id.car_3) {
+                    carSelection = "Car 3";
+                }
+
+                // Determine the selected difficulty
+                String difficulty = "Easy";
+                if (selectedDifficultyId == R.id.difficulty_medium) {
+                    difficulty = "Medium";
+                } else if (selectedDifficultyId == R.id.difficulty_hard) {
+                    difficulty = "Hard";
+                }
+
+                // Start the PlayerDisplayActivity and pass the data
+                Intent GameScreen = new Intent(ChoosePlayer.this, GameScreen.class);
+                GameScreen.putExtra("player_name", playerName);
+                GameScreen.putExtra("car_selection", carSelection);
+                GameScreen.putExtra("difficulty", difficulty);
+                startActivity(GameScreen);
             }
         });
-    }
 
-    public void openGameScreen() {
-        Intent GameScreen = new Intent(this, GameScreen.class);
-        startActivity(GameScreen);
     }
+//    public void openGameScreen() {
+//        Intent GameScreen = new Intent(this, GameScreen.class);
+//        GameScreen.putExtra("player_name", playerName);
+//        GameScreen.putExtra("car_selection", carSelection);
+//        GameScreen.putExtra("difficulty", difficulty);
+//        startActivity(GameScreen);
+//    }
 }
--- app/src/main/java/com/example/crossycar/GameScreen.java
+++ app/src/main/java/com/example/crossycar/GameScreen.java
@@ -3,12 +3,60 @@ package com.example.crossycar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class GameScreen extends AppCompatActivity {
+
+    private TextView playerNameView;
+    //private TextView carSelectionView;
+    //private ImageView[] difficulty = new ImageView[5];
+    private ImageView[] heartIcons = new ImageView[5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+        // Get references to the views
+        playerNameView = findViewById(R.id.player_name);
+        ImageView carIconView = findViewById(R.id.car_icon);
+        heartIcons[0] = findViewById(R.id.heart_icon_1);
+        heartIcons[1] = findViewById(R.id.heart_icon_2);
+        heartIcons[2] = findViewById(R.id.heart_icon_3);
+        heartIcons[3] = findViewById(R.id.heart_icon_4);
+        heartIcons[4] = findViewById(R.id.heart_icon_5);
+
+        // Get the data from the intent
+        String playerName = getIntent().getStringExtra("player_name");
+        String carSelection = getIntent().getStringExtra("car_selection");
+        String difficulty = getIntent().getStringExtra("difficulty");
+
+        // Set the player name
+        playerNameView.setText(playerName);
+
+        // Set the car selection
+        //carSelectionView.setText(carSelection);
+        if (carSelection.equals("Car 1")) {
+            carIconView.setImageResource(R.drawable.bmw);
+        } else if (carSelection.equals("Car 2")) {
+            carIconView.setImageResource(R.drawable.tesla);
+        } else if (carSelection.equals("Car 3")) {
+            carIconView.setImageResource(R.drawable.gwagon);
+        }
+
+        // Show the appropriate number of heart icons based on the difficulty
+        int heartCount = 5;
+        if (difficulty.equals("Medium")) {
+            heartCount = 4;
+        } else if (difficulty.equals("Hard")) {
+            heartCount = 2;
+        }
+        for (int i = 0; i < heartCount; i++) {
+            heartIcons[i].setVisibility(ImageView.VISIBLE);
+        }
+        for (int i = heartCount; i < 5; i++) {
+            heartIcons[i].setVisibility(ImageView.GONE);
+        }
     }
 }
