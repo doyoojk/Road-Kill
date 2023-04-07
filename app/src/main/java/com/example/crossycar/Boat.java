e7b36970454eefa9ae162121f9d0129c917ca807
Jamie Kim <doyoojk@gmail.com>
2023-04-07 14:26:28 -0400

    edit to build.gradle

--- app/src/main/java/com/example/crossycar/Boat.java
+++ app/src/main/java/com/example/crossycar/Boat.java
@@ -1,5 +1,9 @@
 package com.example.crossycar;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
@@ -7,41 +11,70 @@ import android.widget.ImageView;
 
 
 
-public class Boat {
-    private int x;
-    private int y;
+public class Boat implements riverObject{
+    private float x;
+    private float y;
     //private int width, height;
     private int velocity;
+    private int length;
 
 
     //private String name;
 
-    public Boat(int x, int y, int velocity) {
+    public Boat(int x, int y, int velocity, int length) {
         this.x = x;
         this.y = y;
         this.velocity = velocity;
+        this.length = length;
     }
-    public void moveObject(FrameLayout view) {
-        Animation animation = new TranslateAnimation(x, -2200, 0, 0);
-        int duration = 15000 - (velocity * 10);
-        animation.setDuration(duration); // set the duration of the animation (in milliseconds)
-        animation.setRepeatCount(Animation.INFINITE); // set the animation to repeat indefinitely
-        view.startAnimation(animation); // apply the animation to the view
+    @Override
+    public void moveObject(FrameLayout view, int screenW, int delayDist) {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(this, "x", screenW + delayDist, -2200);
+        animator.setDuration(20000 - (velocity * 10)); // set the duration of the animation (in milliseconds)
+        animator.setRepeatCount(ValueAnimator.INFINITE); // set the animation to repeat indefinitely
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                // Update the x-position of the view to match the object
+                view.setX(getX());
+                view.setY(getY());
+            }
+        });
+        animator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                // When the animation ends, set the x-position of the object to the original position
+                setX(screenW + delayDist);
+                // Update the x-position of the view to match the object
+                view.setX(getX());
+                view.setY(getY());
+            }
+        });
+        animator.start(); // start the animation
+        setX(screenW + delayDist); // set the initial x-position of the object
+        setY(getY());
+        view.setX(getX()); // set the x-position of the view to match the object
+        view.setY(getY()); // set the y-position of the view to match the object
     }
 
-    public int getX() {
+
+    public float getX() {
         return x;
     }
-    public int getY() {
+    public float getY() {
         return y;
     }
-    public void setX(int x) {
+    public void setX(float x) {
         x = x;
     }
-    public void setY(int y) {
+    public void setY(float y) {
         y = x;
     }
     public int getVelocity() {
         return velocity;
+    }
+
+    public int getLength() {
+        return length;
     }
 }
